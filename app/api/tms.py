b2de370564a25a19c0447c5358dc4e7864768f86
etech <pythexcel@excellencetechnologies.in>
2019-08-13 18:47:53 +0530

    send message and util changes

--- app/api/tms.py
+++ app/api/tms.py
@@ -7,7 +7,7 @@ from slackclient import SlackClient
 import requests
 import datetime
 from app.config import default
-from app.util import slack_msg, slack_message,serialize_doc
+from app.util import slack_msg, slack_message,serialize_doc,slack_attach
 from flask_jwt_extended import (JWTManager, jwt_required, create_access_token,
                                 get_jwt_identity, get_current_user,
                                 jwt_refresh_token_required,
@@ -15,124 +15,89 @@ from flask_jwt_extended import (JWTManager, jwt_required, create_access_token,
 
 bp = Blueprint('tms', __name__, url_prefix='/tms')
 
-@bp.route('/checkin_send_message', methods=["POST"])
+@bp.route('/send_message', methods=["POST"])
 def post_report():
     if not request.json:
         abort(500)
-    msg_type = request.json.get("type",None)
-    print(msg_type)
-    if msg_type == "Check-in":
-        print("hello")
-        slackReport = request.json.get("slackReport", None)
-        highlight = request.json.get("highlight", "")
-        slackChannels = request.json.get("slackChannels", [])
-        slack = request.json.get("slack", None)
-        date_time = datetime.datetime.utcnow()
-        formatted_date = date_time.strftime("%d-%B-%Y")
-        slack_message(msg="<@" + slack + ">!" + ' '
-                    'have created daily chechk-in at' + ' ' +
-                    str(formatted_date))
-        if len(highlight) > 0:
-            slack_msg(channel=slackChannels,
-                    msg="<@" + slack + ">!" + "\n" + "Report: " + "\n" +
-                    slackReport + "" + "\n" + "Highlight: " + highlight)
-        else:
-            slack_msg(channel=slackChannels,
-                    msg="<@" + slack + ">!" + "\n" + "Report: " + "\n" +
-                    slackReport + "")
-        return jsonify({"msg":"Done"}),200                
-    elif msg_type == "Weekly":
-        print("Nope")
-        slack = request.json.get("slack", None)
-        date_time = datetime.datetime.utcnow()
-        formatted_date = date_time.strftime("%d-%B-%Y")
-        slack_message(msg="<@" + slack + ">!" + ' '
-                    'have created weekly report at' + ' ' +
-                    str(formatted_date))
-        return jsonify({"msg":"Done"}),200            
-    elif msg_type == "Monthly":
-        print("Hope")
-        slack = request.json.get("slack",None)
-        slack_message(msg="<@" + slack + ">!" + ' '
-                                'have created monthly report')
-        return jsonify({"msg":"Done"}),200                        
+    MSG_TYPE = request.json.get("Message_Type",None)
+    print(MSG_TYPE)
+    ret = mongo.db.slack_msg.find_one({"Message_Type": MSG_TYPE})
+    print(ret)
+    if ret is not None:
+        print(ret['Message_Category'])
+        if ret['Message_Category'] == "Simple_Message":
+            slackReport = request.json.get("slackReport", None)
+            highlight = request.json.get("highlight", "")
+            slackChannels = request.json.get("slackChannels", [])
+            slack = request.json.get("slack", None)
+            date_time = datetime.datetime.utcnow()
+            formatted_date = date_time.strftime("%d-%B-%Y")
+            mesg = ret['Message']
+            print(mesg) 
+            if 'Slack_id:' in mesg:
+                print("usme")
+                sl_mesg = mesg.replace("Slack_id:", "<@" + slack + ">!")
+            else:
+                print("isme")
+                sl_mesg = "<@" + slack + ">!" + mesg
+            slack_message(attachments=[{"text": sl_mesg ,"color":ret['Message_Color']}])
+            if len(highlight) > 0:
+                slack_msg(channel=slackChannels,
+                        msg="<@" + slack + ">!",attachments= [{"text": "Report: " + "\n" +
+                        slackReport + "" + "\n" + "Highlight: " + highlight}])
+            else:
+                slack_msg(channel=slackChannels,
+                        msg="<@" + slack + ">!",attachments= [{"text":"Report: " + "\n" +
+                        slackReport,"color":ret['Message_Color']}])
+            return jsonify({"Message":"Sended","Status":True}),200
+        if ret['Category'] == "Button_Message":
+            slack = request.json.get("slack", None) 
+            date_time = datetime.datetime.utcnow()
+            formatted_date = date_time.strftime("%d-%B-%Y")
+            slack_attach(msg="<@" + slack + ">!",attachments=[{"text": ret['Message'] + ' ' +str(formatted_date),"color":ret['Message_Color']}])
+            return jsonify({"Message":"Sended","Status":True}),200
+    else:
+        return jsonify ("No Such Message Type Available"),400
 
-        
-@bp.route('/schdulers_settings', methods=["GET","PUT"])
-@jwt_required
-@token.admin_required
-def schdulers_setings():
-    if request.method == "GET":
-        ret = mongo.db.schdulers_setting.find({
-        })
-        ret = [serialize_doc(doc) for doc in ret]
-        return jsonify(ret)
-
-    if request.method == "PUT":
-        monthly_remainder = request.json.get("monthly_remainder")
-        weekly_remainder = request.json.get("weekly_remainder")
-        recent_activity = request.json.get("recent_activity")
-        review_activity = request.json.get("review_activity")
-        monthly_manager_reminder = request.json.get("monthly_manager_reminder")
-        revew_360_setting=request.json.get("revew_360_setting",True)
-        missed_reviewed=request.json.get("missed_reviewed",True)
-        skip_review_setting=request.json.get("managerSkip",True)
-        weekly_automated=request.json.get("weekly_automated",True)
-        ret = mongo.db.schdulers_setting.update({
-            },{
-                "$set":{
-                "monthly_remainder": monthly_remainder,
-                "weekly_remainder": weekly_remainder,
-                "recent_activity": recent_activity,
-                "review_activity": review_activity,
-                "monthly_manager_reminder": monthly_manager_reminder,
-                "revew_360_setting":revew_360_setting,
-                "missed_reviewed":missed_reviewed,
-                "skip_review":skip_review_setting,
-                "weekly_automated":weekly_automated
-            }}, upsert=True)
-        return jsonify(str(ret))
-
-        
-        #Api for schdulers mesg settings
-@bp.route('/schduler_mesg', methods=["GET","PUT"])
+            
+#Api for schdulers mesg settings
+@bp.route('/slack_mesg', methods=["GET","PUT"])
 @jwt_required
 @token.admin_required
 def slack_schduler():
     if request.method == "GET":
-        ret = mongo.db.schdulers_msg.find({
+        ret = mongo.db.slack_msg.find({
         })
         ret = [serialize_doc(doc) for doc in ret]
-        return jsonify([default] if not ret else ret)
+        return jsonify(ret)
     if request.method == "PUT":
-        monthly_remainder = request.json.get("monthly_remainder")
-        weekly_remainder1 = request.json.get("weekly_remainder1")
-        weekly_remainder2 = request.json.get("weekly_remainder2")
-        review_activity = request.json.get("review_activity")
-        monthly_manager_reminder = request.json.get("monthly_manager_reminder")
-        missed_checkin = request.json.get("missed_checkin")
-        weekly_report_mesg=request.json.get("weekly_report_mesg")
-        monthly_report_mesg=request.json.get("monthly_report_mesg")
-        missed_review_msg = request.json.get("missed_reviewed_mesg")
-        ret = mongo.db.schdulers_msg.update({
+        MSG = request.json.get("Message",None)
+        MSG_TYPE = request.json.get("Message_Type",None)
+        MSG_ORIGIN = request.json.get("Message_Origin",None)
+        Category = request.json.get("Message_Category",None)
+        MSG_Color = request.json.get("Message_Color",None)
+        Working = request.json.get("Working",True)
+        unique_key = request.json.get("Unique_key")
+        
+        if not MSG and MSG_TYPE and MSG_ORIGIN:
+            return jsonify({"msg": "Invalid Request"}), 400
+    
+        ret = mongo.db.slack_msg.update({
         }, {
             "$set": {
-                "monthly_remainder": monthly_remainder,
-                "weekly_remainder1": weekly_remainder1,
-                "weekly_remainder2":weekly_remainder2,
-                "review_activity":review_activity,
-                "monthly_manager_reminder":monthly_manager_reminder,
-                "missed_checkin":missed_checkin,
-                "weekly_report_mesg":weekly_report_mesg,
-                "monthly_report_mesg":monthly_report_mesg,
-                "missed_reviewed_mesg":missed_review_msg
+                "Message":  MSG,
+                "Message_Type": MSG_TYPE,
+                "Message_Origin":MSG_ORIGIN,
+                "Message_Category":Category,
+                "Unique_key":unique_key,
+                "Message_Color":MSG_Color
             }
         },upsert=True)
         return jsonify(str(ret))
 
+
     
-        
-    
+
 @bp.route('/tms_settings', methods=["PUT", "GET"])
 @jwt_required
 @token.admin_required
@@ -154,4 +119,5 @@ def tms_setings():
             }
         },upsert=True)
         return jsonify(str(ret))
+
     
--- app/util.py
+++ app/util.py
@@ -9,7 +9,7 @@ def serialize_doc(doc):
     return doc
 
 def secret_key():
-    msg = mongo.db.slack_tokens.find_one({
+    msg = mongo.db.tms_settings.find_one({
         "secret_key": {"$exists": True}
     }, {"secret_key": 1, '_id': 0})
     secret_key = msg['secret_key']
@@ -17,16 +17,16 @@ def secret_key():
 
 
 #Function for find webhook_url
-def load_hook():
-    url = mongo.db.slack_tokens.find_one({
+def tms_load_hook():
+    url = mongo.db.tms_settings.find_one({
         "webhook_url": {"$exists": True}
     }, {"webhook_url": 1, '_id': 0})
     web_url = url['webhook_url']
     return web_url
 #function for send mesg 
-def slack_message(msg):
-    slackmsg = {"text": msg}
-    webhook_url = load_hook()
+def slack_message(msg=None,attachments=None):
+    slackmsg = {"text": msg,"attachments":attachments}
+    webhook_url = tms_load_hook()
     response = requests.post(
         webhook_url, json=slackmsg,
         headers={'Content-Type': 'application/json'})
@@ -52,21 +52,22 @@ def slack_attach(msg):
         headers={'Content-Type': 'application/json'})
 
 #function for find slack_token
-def load_token():
-    token = mongo.db.slack_tokens.find_one({
+def tms_load_token():
+    token = mongo.db.tms_settings.find_one({
         "slack_token": {"$exists": True}
     }, {"slack_token": 1, '_id': 0})
     sl_token = token['slack_token']
     return sl_token
 
-def slack_msg(channel, msg):
-    slack_token = load_token()
+def slack_msg(channel, msg,attachments):
+    slack_token = tms_load_token()
     sc = SlackClient(slack_token)
     for data in channel:
         sc.api_call(
             "chat.postMessage",
             channel=data,
             text=msg,
+            attachments=attachments,
             username = "TMS"
         )
 
