102b514621eed6d31f2a3bf28c002d35d564569f
etech <pythexcel@excellencetechnologies.in>
2019-12-16 16:37:40 +0530

    print removed

--- app/__init__.py
+++ app/__init__.py
@@ -46,7 +46,6 @@ def create_app(test_config=None):
     app.config['to'] = os.getenv('cc')
     app.config['to'] = os.getenv('bcc')
     
-    print(app.config)
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
