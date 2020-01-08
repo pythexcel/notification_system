e4012bef0a41cd7768366439c455a6000940a5bf
etech <pythexcel@excellencetechnologies.in>
2020-01-08 17:10:39 +0530

    smtp changes

--- app/api/mail_settings.py
+++ app/api/mail_settings.py
@@ -8,35 +8,64 @@ from flask_jwt_extended import (
     get_jwt_identity, get_current_user, jwt_refresh_token_required,
     verify_jwt_in_request
 )
+from flask import current_app as app
 
 
 bp = Blueprint('mail_settings', __name__, url_prefix='/smtp')
 
-@bp.route('/settings', methods=["PUT", "GET"])
+@bp.route('/settings/<string:origin>', methods=["PUT", "GET"])
 # @token.admin_required
-def mail_setings():
+def mail_setings(origin):
     if request.method == "GET":
-       mail = mongo.db.mail_settings.find_one({},{"_id":0,"mail_password":0})
+       mail = mongo.db.mail_settings.find({"origin":origin},{"mail_password":0})
+       mail = [serialize_doc(doc) for doc in mail]
        return jsonify (mail)
     if request.method == "PUT":
         if not request.json:
             abort(500)
-        mail_server = request.json.get("mail_server", None)
-        mail_port = request.json.get("mail_port", 0)
-        mail_use_tls = request.json.get("mail_use_tls", True)
-        mail_username = request.json.get("mail_username", None)
-        mail_password = request.json.get("mail_password", None)
-        
-        if not mail_server and mail_password and mail_port and mail_use_tls and mail_username:
-            return jsonify({"msg": "Invalid Request"}), 400    
-        
-        ret = mongo.db.mail_settings.update({}, {
-            "$set": {
-                "mail_server": mail_server,
-                "mail_port": mail_port,
-                "mail_use_tls": mail_use_tls,
-                "mail_username":mail_username,
-                "mail_password":mail_password
-            }
-        },upsert=True)
-        return jsonify({"MSG":"upsert"}),200
+        if origin == "HR":    
+            #checking origin of api hit so if it is HR one smtp conf can be created or updated only
+            mail_server = request.json.get("mail_server", None)
+            mail_port = request.json.get("mail_port", 0)
+            mail_use_tls = request.json.get("mail_use_tls", True)
+            mail_username = request.json.get("mail_username", None)
+            mail_password = request.json.get("mail_password", None)
+            
+            if not mail_server and mail_password and mail_port and mail_use_tls and mail_username:
+                return jsonify({"msg": "Invalid Request"}), 400    
+            
+            ret = mongo.db.mail_settings.update({}, {
+                "$set": {
+                    "mail_server": mail_server,
+                    "mail_port": mail_port,
+                    "origin": origin,
+                    "mail_use_tls": mail_use_tls,
+                    "mail_username":mail_username,
+                    "mail_password":mail_password
+                }
+            },upsert=True)
+            return jsonify({"MSG":"upsert"}),200
+        elif origin == "RECRUIT":
+            #checking origin of api hit so if it is RECRUIT multiple smtp conf can be created 
+            mail_server = request.json.get("mail_server", None)
+            mail_port = request.json.get("mail_port", 0)
+            mail_use_tls = request.json.get("mail_use_tls", True)
+            mail_username = request.json.get("mail_username", None)
+            mail_password = request.json.get("mail_password", None)
+            active = request.json.get("active",True)
+            
+            if not mail_server and mail_password and mail_port and mail_use_tls and mail_username:
+                return jsonify({"msg": "Invalid Request"}), 400    
+            
+            ret = mongo.db.mail_settings.update({"mail_server": mail_server,"origin": "RECRUIT"}, {
+                "$set": {
+                    "mail_server": mail_server,
+                    "mail_port": mail_port,
+                    "origin": origin,
+                    "mail_use_tls": mail_use_tls,
+                    "mail_username":mail_username,
+                    "mail_password":mail_password,
+                    "active": active
+                }
+            },upsert=True)
+            return jsonify({"MSG":"upsert"}),200
--- app/mail_util.py
+++ app/mail_util.py
@@ -8,11 +8,25 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import email.mime.application
 import mimetypes
+from flask import current_app as app
 
-def send_email(message,recipients,subject,bcc=None,cc=None,filelink=None,filename=None,link=None):
-    mail_details = mongo.db.mail_settings.find_one({},{"_id":0})
-    username = mail_details["mail_username"]
-    password = mail_details["mail_password"]
+def send_email(message,recipients,subject,bcc=None,cc=None,filelink=None,filename=None,link=None,sending_mail=None,sending_password=None):
+    # again below checking origin condition as this function sends mail so need to check and select right smtp for single mail sending
+    if app.config['origin'] == "hr":
+        mail_details = mongo.db.mail_settings.find_one({"origin": "HR"},{"_id":0})
+    elif app.config['origin'] == "recruit":    
+        mail_details = mongo.db.mail_settings.find_one({"origin": "RECRUIT"},{"_id":0})
+    username = None
+    if sending_mail is None:    
+        username = mail_details["mail_username"]
+    else:
+        username = sending_mail 
+    password = None
+    #below is written for recruit condition for multiple smtp as need to iterate over values from campaign schduler
+    if sending_password is None:       
+        password = mail_details["mail_password"]
+    else:
+        password = sending_password       
     port = mail_details['mail_port']
     mail_server = mail_details['mail_server']
     mail = smtplib.SMTP_SSL(str(mail_server), port)
