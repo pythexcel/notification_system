d959ccea3124d40c18449929d7ef45baf6ffa7f0
etech <pythexcel@excellencetechnologies.in>
2020-01-27 18:43:43 +0530

    hr validation

--- app/api/mail_settings.py
+++ app/api/mail_settings.py
@@ -58,19 +58,32 @@ def mail_setings(origin,id=None):
             
             if not mail_server and mail_password and mail_port and mail_use_tls and mail_username:
                 return jsonify({"msg": "Invalid Request"}), 400    
-            if origin == "HR":    
-                ret = mongo.db.mail_settings.update({}, {
-                    "$set": {
-                        "mail_server": mail_server,
-                        "mail_port": mail_port,
-                        "origin": origin,
-                        "mail_use_tls": mail_use_tls,
-                        "mail_username":mail_username,
-                        "mail_password":mail_password,
-                        "mail_from": mail_from
-                    }
-                },upsert=True)
-                return jsonify({"MSG":"upsert"}),200
+            if origin == "HR": 
+                try:
+                    send_email(message="SMTP WORKING!",recipients=[email],subject="SMTP TESTING MAIL!",sending_mail=mail_username,sending_password=mail_password,sending_port=mail_port,sending_server=mail_server)
+                except smtplib.SMTPServerDisconnected:
+                    return jsonify({"MSG": "Smtp server is disconnected"}), 400                
+                except smtplib.SMTPConnectError:
+                    return jsonify({"MSG": "Smtp is unable to established"}), 400    
+                except smtplib.SMTPAuthenticationError:
+                    return jsonify({"MSG": "Smtp login and password is wrong"}), 400                           
+                except smtplib.SMTPDataError:
+                    return jsonify({"MSG": "Smtp account is not activated"}), 400 
+                except Exception:
+                    return jsonify({"MSG": "Something went wrong with smtp"}), 400
+                else:   
+                    ret = mongo.db.mail_settings.update({}, {
+                        "$set": {
+                            "mail_server": mail_server,
+                            "mail_port": mail_port,
+                            "origin": origin,
+                            "mail_use_tls": mail_use_tls,
+                            "mail_username":mail_username,
+                            "mail_password":mail_password,
+                            "mail_from": mail_from
+                        }
+                    },upsert=True)
+                    return jsonify({"MSG":"upsert"}),200
 
             elif origin == "RECRUIT":
                 email = app.config['to']
