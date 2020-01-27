94981e29879b2d27b414a5a1026297619420f08b
etech <pythexcel@excellencetechnologies.in>
2020-01-27 18:34:42 +0530

    fix for smtp api

--- app/api/mail_settings.py
+++ app/api/mail_settings.py
@@ -46,7 +46,6 @@ def mail_setings(origin,id=None):
     if request.method == "POST":
         if not request.json:
             abort(500)
-        if origin == "HR":    
             #checking origin of api hit so if it is HR one smtp conf can be created or updated only
             mail_server = request.json.get("mail_server", "smtp.gmail.com")
             mail_port = request.json.get("mail_port", 465)
@@ -54,47 +53,42 @@ def mail_setings(origin,id=None):
             mail_username = request.json.get("mail_username", None)
             mail_password = request.json.get("mail_password", None)
             mail_from  = request.json.get("mail_from", None)
-            
-            
-            if not mail_server and mail_password and mail_port and mail_use_tls and mail_username:
-                return jsonify({"msg": "Invalid Request"}), 400    
-                    
-            ret = mongo.db.mail_settings.update({}, {
-                "$set": {
-                    "mail_server": mail_server,
-                    "mail_port": mail_port,
-                    "origin": origin,
-                    "mail_use_tls": mail_use_tls,
-                    "mail_username":mail_username,
-                    "mail_password":mail_password,
-                    "mail_from": mail_from
-                }
-            },upsert=True)
-            return jsonify({"MSG":"upsert"}),200
-        elif origin == "RECRUIT":
-            #checking origin of api hit so if it is RECRUIT multiple smtp conf can be created 
-            mail_server = request.json.get("mail_server", None)
-            mail_port = request.json.get("mail_port", 0)
-            mail_use_tls = request.json.get("mail_use_tls", True)
-            mail_username = request.json.get("mail_username", None)
-            mail_password = request.json.get("mail_password", None)
             active = request.json.get("active",True)
-            mass = request.json.get("mass",False)
             type_s = request.json.get("type", "tls")
             
             if not mail_server and mail_password and mail_port and mail_use_tls and mail_username:
-                return jsonify({"msg": "Invalid Request"}), 400  
-            email = app.config['to']
-            smtp_right = True
-            unregister = False
-            try:
-                send_email(message="SMTP WORKING!",recipients=[email],subject="SMTP TESTING MAIL!",sending_mail=mail_username,sending_password=mail_password,sending_port=mail_port,sending_server=mail_server)
-            except smtplib.SMTPDataError:
-                unregister = True
-            except Exception:    
-                smtp_right = False
-            if unregister is False:    
-                if smtp_right is True:                     
+                return jsonify({"msg": "Invalid Request"}), 400    
+            if origin == "HR":    
+                ret = mongo.db.mail_settings.update({}, {
+                    "$set": {
+                        "mail_server": mail_server,
+                        "mail_port": mail_port,
+                        "origin": origin,
+                        "mail_use_tls": mail_use_tls,
+                        "mail_username":mail_username,
+                        "mail_password":mail_password,
+                        "mail_from": mail_from
+                    }
+                },upsert=True)
+                return jsonify({"MSG":"upsert"}),200
+
+            elif origin == "RECRUIT":
+                email = app.config['to']
+                smtp_right = True
+                unregister = False
+                try:
+                    send_email(message="SMTP WORKING!",recipients=[email],subject="SMTP TESTING MAIL!",sending_mail=mail_username,sending_password=mail_password,sending_port=mail_port,sending_server=mail_server)
+                except smtplib.SMTPServerDisconnected:
+                    return jsonify({"MSG": "Smtp server is disconnected"}), 400                
+                except smtplib.SMTPConnectError:
+                    return jsonify({"MSG": "Smtp is unable to established"}), 400    
+                except smtplib.SMTPAuthenticationError:
+                    return jsonify({"MSG": "Smtp login and password is wrong"}), 400                           
+                except smtplib.SMTPDataError:
+                    return jsonify({"MSG": "Smtp account is not activated"}), 400 
+                except Exception:
+                    return jsonify({"MSG": "Something went wrong with smtp"}), 400  
+                else:           
                     vet = mongo.db.mail_settings.find_one({"mail_username":mail_username,
                             "mail_password":mail_password,"origin":origin})
                     if vet is None:
@@ -112,13 +106,9 @@ def mail_setings(origin,id=None):
                                 "mail_password":mail_password,
                                 "active": active,
                                 "type": type_s,
-                                "mass": mass
+                                "mail_from": mail_from
                         })
                         return jsonify({"MSG":"upsert"}),200
                     else:
                         return jsonify({"MSG":"Smtp already exists"}),400
-                else:
-                    return jsonify({"MSG":"Invalid SMTP"}),400
-            else:
-                return jsonify({"MSG":"Mail account in not registered"}),400
-
+                    
