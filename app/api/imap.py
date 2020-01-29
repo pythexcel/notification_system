a304939f8b82e447b2be17a9ed286c967706fd2b
aayush <rasealex000000@gmail.com>
2020-01-29 12:03:33 +0530

    chat sequenced

--- app/api/imap.py
+++ app/api/imap.py
@@ -8,6 +8,7 @@ import email
 import datetime
 import pprint
 import re
+from dateutil.parser import parse
 
 
 
@@ -22,34 +23,37 @@ def get_emails():
         mail_username = request.json.get("mail_username",None)
         since = request.json.get("since",None)
         smtp_values = mongo.db.mail_settings.find_one({"mail_username":mail_username,"active":True})
-        mail_password = smtp_values['mail_password']
-        if imap_server and mail_username is not None:
-            try:
-                print("trying login")
-                imapObj = imapclient.IMAPClient(imap_server, ssl=True)
-                imapObj.login(mail_username,mail_password)
-            except Exception:
-                return jsonify({"error":"IMAP credentials are not valid"}),400
-            else:
-                print("login successfully")
-                imapObj.select_folder('INBOX')
-                search_bounce_mails=imapObj.search(['SINCE',since])
-                emails = []
-                if search_bounce_mails:
-                    for search_bounce_mail in search_bounce_mails:
-                        rawMessages = imapObj.fetch(search_bounce_mail,['BODY[]'])
-                        message_body = pyzmail.PyzMessage.factory(rawMessages[search_bounce_mail][b'BODY[]'])
-                        mail_subject = message_body.get_subject()
-                        mail_from =message_body.get_address('from')[1]
-                        mail_to =message_body.get_address('to')[1]
-                        date =message_body.get_decoded_header('date')
-                        if {"mail_from":mail_from} not in emails:
-                            emails.append({"mail_from":mail_from})
-                    return jsonify(emails),200
+        if smtp_values is not None:
+            mail_password = smtp_values['mail_password']
+            if imap_server and mail_username is not None:
+                try:
+                    print("trying login")
+                    imapObj = imapclient.IMAPClient(imap_server, ssl=True)
+                    imapObj.login(mail_username,mail_password)
+                except Exception as e:
+                    return jsonify({"error":e}),400
                 else:
-                    return jsonify({"error":"No mails available from this date"}),400
+                    print("login successfully")
+                    imapObj.select_folder('INBOX')
+                    search_bounce_mails=imapObj.search(['SINCE',since])
+                    emails = []
+                    if search_bounce_mails:
+                        for search_bounce_mail in search_bounce_mails:
+                            rawMessages = imapObj.fetch(search_bounce_mail,['BODY[]'])
+                            message_body = pyzmail.PyzMessage.factory(rawMessages[search_bounce_mail][b'BODY[]'])
+                            mail_subject = message_body.get_subject()
+                            mail_from =message_body.get_address('from')[1]
+                            mail_to =message_body.get_address('to')[1]
+                            date =message_body.get_decoded_header('date')
+                            if {"mail_from":mail_from} not in emails:
+                                emails.append({"mail_from":mail_from})
+                        return jsonify(emails),200
+                    else:
+                        return jsonify({"error":"No mails available from this date"}),400
+            else:
+                return jsonify({"error":"Payload are not valid somthing missing"}),400
         else:
-            return jsonify({"error":"Payload are not valid somthing missing"}),400
+            return jsonify({"error":"This username is not Available"}),400
     else:
         return jsonify({"error":"Method not allowed"}),400
     
@@ -62,37 +66,43 @@ def get_chats():
         mail_username = request.json.get("mail_username",None)
         chat_email = request.json.get("chat_email",None)
         smtp_values = mongo.db.mail_settings.find_one({"mail_username":mail_username,"active":True})
-        mail_password = smtp_values['mail_password']
-        if imap_server and mail_username and chat_email is not None:
-            try:
-                print("trying login")
-                imapObj = imapclient.IMAPClient(imap_server, ssl=True)
-                imapObj.login(mail_username,mail_password)
-            except Exception:
-                return jsonify({"error":"IMAP credentials are not valid"}),400
-            else:
-                print("login successfully")
-                imapObj.select_folder('INBOX')
-                sended_ma=imapObj.search(['FROM',mail_username,'TO',chat_email])
-                recieved_mails = imapObj.search(['FROM',chat_email,'TO',mail_username])
-                sended_mails = sended_ma + recieved_mails
-                emails = []
-                if sended_mails:
-                    for sended_mail in sended_mails:
-                        rawMessages = imapObj.fetch(sended_mail,['BODY[]'])
-                        message_body = pyzmail.PyzMessage.factory(rawMessages[sended_mail][b'BODY[]'])
-                        mail_subject = message_body.get_subject()
-                        mail_from =message_body.get_address('from')[1]
-                        mail_to =message_body.get_address('to')[1]
-                        date =message_body.get_decoded_header('date')
-                        if message_body.text_part.charset != None: #checking if msg body have text part
-                            mail_text = message_body.text_part.get_payload().decode(message_body.text_part.charset)
-                        emails.append({"mail_subject":mail_subject,"mail_from":mail_from,"mail_to":mail_to,"datetime":date,"message":mail_text})
-                    return jsonify(emails),200
+        if smtp_values is not None:
+            mail_password = smtp_values['mail_password']
+            if imap_server and mail_username and chat_email is not None:
+                try:
+                    print("trying login")
+                    imapObj = imapclient.IMAPClient(imap_server, ssl=True)
+                    imapObj.login(mail_username,mail_password)
+                except Exception as e:
+                    return jsonify({"error":e}),400
                 else:
-                    return jsonify({"error":"No mails available from this date"}),400
+                    print("login successfully")
+                    imapObj.select_folder('INBOX')
+                    sended_ma=imapObj.search(['FROM',mail_username,'TO',chat_email])
+                    recieved_mails = imapObj.search(['FROM',chat_email,'TO',mail_username])
+                    sended_mails = sended_ma + recieved_mails
+                    emails = []
+                    if sended_mails:
+                        for sended_mail in sended_mails:
+                            rawMessages = imapObj.fetch(sended_mail,['BODY[]'])
+                            message_body = pyzmail.PyzMessage.factory(rawMessages[sended_mail][b'BODY[]'])
+                            mail_subject = message_body.get_subject()
+                            mail_from =message_body.get_address('from')[1]
+                            mail_to =message_body.get_address('to')[1]
+                            date =message_body.get_decoded_header('date')
+                            dt = parse(date)
+                            mail_time=dt.strftime("%b %d %Y %H:%M:%S")
+                            if message_body.text_part.charset != None: #checking if msg body have text part
+                                mail_text = message_body.text_part.get_payload().decode(message_body.text_part.charset)
+                            emails.append({"mail_subject":mail_subject,"mail_from":mail_from,"mail_to":mail_to,"datetime":mail_time,"message":mail_text})
+                        sortedArray = sorted(emails,key=lambda x: datetime.datetime.strptime(x['datetime'],"%b %d %Y %H:%M:%S"), reverse=True)
+                        return jsonify(sortedArray),200
+                    else:
+                        return jsonify({"error":"No mails available from this date"}),400
+            else:
+                return jsonify({"error":"Payload are not valid somthing missing"}),400
         else:
-            return jsonify({"error":"Payload are not valid somthing missing"}),400
+            return jsonify({"error":"Username is not available in DB"}),400
     else:
         return jsonify({"error":"Method not allowed"}),400
 
