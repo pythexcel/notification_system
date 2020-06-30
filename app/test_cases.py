406cff81169ca90560f769d4c18d2cd63b8161a5
pythexcel <pythexcel@excellencetechnologies.in>
2020-06-30 16:28:47 +0530

    test cases

---
+++ app/test_cases.py
@@ -0,0 +1,30 @@
+from app import create_app as app
+from app.util import allowed_file,FetchRecipient
+from app.modules.phone_util import create_sms
+
+
+#Test case for create mobile message
+def test_create_sms():
+    phone = "8445679788"
+    mobile_message_str = "testing"
+    assert phone and mobile_message_str is not None 
+    status = create_sms(phone=phone, mobile_message_str=mobile_message_str)
+    assert status
+
+
+
+#Test for check file type is valid or not
+def test_filename():
+    filename = "testing.pdf"
+    assert type(filename) == str
+    file = allowed_file(filename=filename)
+    assert file != None
+
+
+#test for fetch recipients from user details
+def test_FetchRecipient():
+    slack_user_detail = {"email_group":["testing1@gmail.com","testing2@gmail.com"]}
+    message_detail = {"email_group":["testing3@gmail.com","testing4@gmail.com"]}
+    assert 'email_group' in message_detail
+    status = FetchRecipient(slack_user_detail=slack_user_detail,message_detail=message_detail)
+    assert type(status) == list
