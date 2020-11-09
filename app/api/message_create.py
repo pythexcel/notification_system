5b02f7bebd874e2dca0e0424ea91de3b1a76bb11
pythexcel <pythexcel@excellencetechnologies.in>
2020-11-09 12:58:41 +0530

    added hold trigger

--- app/api/message_create.py
+++ app/api/message_create.py
@@ -411,7 +411,7 @@ def slack_channel_test():
 def get_triggers():
     duplicate = []
     triggers = []
-
+    holdTriger = ["when candidate is on hold"]
     ret = mongo.db.mail_template.find({"message_origin": "RECRUIT"})
     ret = [serialize_doc(doc) for doc in ret]
     if ret:
@@ -420,7 +420,7 @@ def get_triggers():
     for elem in duplicate:
         if elem not in triggers:
             triggers.append(elem)
-    return jsonify({"triggers": triggers}), 200
+    return jsonify({"triggers": triggers+holdTriger}), 200
 
 
 #Api for update channel code for all messages.
