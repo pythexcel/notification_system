aab14a0a459b9bda2650d4792495f7d10c49685f
Your Name <you@example.com>
2021-04-09 11:07:13 +0530

    local

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf gunicorn-nf--access.log.*
 rm -rf gunicorn-nf--error.log.*
-git pull origin dev
+git pull origin dev_branch
 source ../myenv/bin/activate
 pip install -r requirements.txt
 sudo supervisorctl restart hr-staging-notify
