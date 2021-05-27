47d8c389744a4783f30b03d7166f8607ca4e2e29
pythexcel <pythexcel@excellencetechnologies.in>
2021-05-27 16:41:46 +0530

    fxied

--- app/api/message_create.py
+++ app/api/message_create.py
@@ -145,6 +145,10 @@ def mail_message(message_origin):
         working = True
         if "reminder" in request.form:
             reminder = request.form["reminder"]
+            if reminder == "true":
+                reminder = True
+            else:
+                reminder = False
         else:
             reminder =False
         if "working" in request.form:
@@ -160,9 +164,14 @@ def mail_message(message_origin):
         default = False
         if "default" in request.form:
             default = request.form["default"]
+            if default == "true":
+                default = True
+            else:
+                default = False
             
         if not MSG and MSG_KEY and message_origin and MSG_SUBJECT:
             return jsonify({"MSG": "Invalid Request"}), 400
+        JobProfileId = None
         if "JobProfileId" in request.form:
             JobProfileId = request.form["JobProfileId"]
             ver = mongo.mail_template.find_one({"JobProfileId": JobProfileId,"message_key": MSG_KEY})
